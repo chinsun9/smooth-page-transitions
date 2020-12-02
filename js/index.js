ebfb703c608a30935aec5267c22798c496f241c7
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-12-02 14:34:25 +0900

    Transition

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-const run = () => {
-  console.log("yo i am runnin");
-};
+import Highway from "@dogstudio/highway";
+import Fade, { Fade2 } from "./transition";
+
+const H = new Highway.Core({
+  transitions: {
+    default: Fade,
+    about: Fade2,
+    contact: Fade2,
+  },
+});
---
+++ js/transition.js
@@ -0,0 +1,67 @@
+import Highway from "@dogstudio/highway";
+import { TimelineLite } from "gsap";
+
+class Fade extends Highway.Transition {
+  in({ from, to, done }) {
+    const tl = new TimelineLite();
+    tl.fromTo(to, { left: "-100%", top: "50%" }, { left: "0%", duration: 0.5 })
+      .fromTo(
+        to,
+        { height: "2vh" },
+        {
+          height: "90vh",
+          top: "10vh",
+          duration: 0.5,
+          onComplete: () => {
+            from.remove();
+            done();
+          },
+        }
+      )
+      .fromTo(to.children[0], { opacity: 0 }, { opacity: 1, duration: 0.7 });
+  }
+  out({ done }) {
+    done();
+  }
+}
+
+class Fade2 extends Highway.Transition {
+  in({ from, to, done }) {
+    const tl = new TimelineLite();
+    tl.fromTo(
+      to,
+      {
+        left: "50%",
+        top: "50%",
+        width: "0%",
+        height: "0%",
+        borderRadius: "50%",
+      },
+      {
+        left: "0%",
+        top: "10%",
+        width: "100%",
+        height: "90vh",
+        duration: 0.5,
+        onComplete: () => {
+          from.remove();
+          done();
+        },
+      }
+    )
+      .fromTo(
+        to,
+        { borderRadius: "50%" },
+        { borderRadius: "0%", duration: 0.6 },
+        +0.2
+      )
+      .fromTo(to.children[0], { opacity: 0 }, { opacity: 1, duration: 0.5 });
+  }
+  out({ done }) {
+    done();
+  }
+}
+
+export default Fade;
+
+export { Fade2 };
